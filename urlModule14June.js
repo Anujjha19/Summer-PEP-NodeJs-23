642f57df6b7d58fd1349dce42b05f9ea887b9d4b
Anuj Jha <anujjha710@gmail.com>
2023-06-16 10:21:50 +0530

    14 June

---
+++ urlModule14June.js
@@ -0,0 +1,4 @@
+var url = require('url')
+var address = "https://lovelyprofessionaluniversity.codetantra.com/secure/tla/jnr.jsp?m=ab029b4a-6050-3b81-bcb3-4e572bb854d7&r"
+
+console.log(url.parse(address, true))
