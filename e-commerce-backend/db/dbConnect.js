875df51ba218732f1b65f75350b996c37ca3ed71
Anuj Jha <78872834+Anujjha19@users.noreply.github.com>
2023-07-07 10:15:47 +0530

    Update dbConnect.js

--- e-commerce-backend/db/dbConnect.js
+++ e-commerce-backend/db/dbConnect.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 async function dbConnect() {
-  await mongoose.connect(process.env.DB_URL, { dbName: "flip-zon" });
+  await mongoose.connect(process.env.DB_URL, { dbName: "ecom-zon" });
   console.log("successfully connected to MongoDB.");
 }
 
